2959ca13fb3867632e905802650652354985d2cc
Alex Orange <alex.orange.stockmouton@gmail.com>
2021-10-05 18:26:34 +0200

    Fix deployment

--- deploy/deploy-lenia-contract.js
+++ deploy/deploy-lenia-contract.js
@@ -8,8 +8,18 @@ const deployLeniaContract = async hre => {
     from: deployer,
     log: true, 
   });
-
+  
+  // const otherAddresses = otherAccounts.map(account => account.address)
+  // // Simulate splitting Ether balance among a group of accounts
+  // const payeeAdresses = [
+  //     owner.address, // StockMouton DAO
+  //     otherAddresses[0], // Team Member 1
+  //     otherAddresses[1], // Team Member 2
+  //     otherAddresses[2], // Team Member 3
+  // ]
+  // const payeeShares = [450, 225, 225, 100]
   await deploy('Lenia', {
+    args: [[deployer], [1]],
     from: deployer,
     log: true, 
     libraries: {
