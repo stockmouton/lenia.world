50da79ef3a0044097d5462600ab3ff1c451dce5f
Alex Orange <alex.orange.stockmouton@gmail.com>
2021-10-12 11:00:31 +0200

    Create LeniaRoyalties contract infra

--- tasks/verify.js
+++ tasks/verify.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 
-task("verify-osef", "Set the engine in the smart contract",  async (taskArgs, hre) => {
+task("verify-lenia", "Verify Lenia smart contract",  async (taskArgs, hre) => {
   const LeniaDescriptorLibraryDeployment = await hre.deployments.get('LeniaDescriptor')
   const LeniaDeployment = await hre.deployments.get('Lenia')
 
@@ -14,13 +14,34 @@ task("verify-osef", "Set the engine in the smart contract",  async (taskArgs, hr
   }
 
   await hre.run("verify:verify", {
-  address: LeniaDeployment.address,
-  constructorArguments: [
-    addresses,
-    shares
-  ],
-  libraries: {
-    LeniaDescriptor: LeniaDescriptorLibraryDeployment.address,
+    address: LeniaDeployment.address,
+    constructorArguments: [
+      addresses,
+      shares
+    ],
+    libraries: {
+      LeniaDescriptor: LeniaDescriptorLibraryDeployment.address,
+    }
+  })
+})
+
+task("verify-royalties", "Verify LeniaRoyalties smart contract",  async (taskArgs, hre) => {
+  const LeniaRoyaltiesDeployment = await hre.deployments.get('LeniaRoyalties')
+
+  const addresses = []
+  const shares = []
+  splitter = JSON.parse(fs.readFileSync(path.join(__dirname, `../tasks/data/royalties-payment-splitter-${hre.hardhatArguments.network}.json`), 'utf8'))
+  for (i = 0; i < splitter.length; i++) {
+    addresses.push(splitter[i].address)
+    shares.push(splitter[i].shares)
   }
-});
+
+  await hre.run("verify:verify", {
+    address: LeniaRoyaltiesDeployment.address,
+    constructorArguments: [
+      addresses,
+      shares
+    ],
+    contract: "contracts/LeniaRoyalties.sol:LeniaRoyalties"
+  })
 })
