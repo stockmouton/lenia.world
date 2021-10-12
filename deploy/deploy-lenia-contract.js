127531fadca67c988603f5bf741093ccab0c10d9
Morgan Giraud <contact@morgangiraud.com>
2021-10-12 11:44:41 +0200

    Fix local deploy

--- deploy/deploy-lenia-contract.js
+++ deploy/deploy-lenia-contract.js
@@ -1,15 +1,17 @@
 const prompt = require('prompt');
 const { assert } = require("chai")
 const fs = require("fs")
-const path = require("path")
+const path = require("path");
+const { solidityKeccak256 } = require('ethers/lib/utils');
 
 const deployLeniaContract = async hre => {
   const {deployments, getNamedAccounts} = hre
   const {deploy} = deployments
   
   const accounts = await getNamedAccounts()
-  const addresses = []
-  const shares = []
+  const deployer = accounts.deployer
+  let addresses = []
+  let shares = []
 
   if (hre.hardhatArguments.network == 'mainnet' || hre.hardhatArguments.network == 'rinkeby') {
     splitter = JSON.parse(fs.readFileSync(path.join(__dirname, `../tasks/data/payment-splitter-${hre.hardhatArguments.network}.json`), 'utf8'))
@@ -17,22 +19,27 @@ const deployLeniaContract = async hre => {
       addresses.push(splitter[i].address)
       shares.push(splitter[i].shares)
     }
-  } else {
-    //addresses = await getNamedAccounts()
-  }
+    assert(Object.keys(addresses).length == 4, 'Expecting 4 addresses to deploy on mainnet')
+
+    console.log('Deploying Lenia Contract')
+    console.log('Splitees', addresses)
+    console.log('Shares', shares)
     
-  assert(Object.keys(addresses).length == 4, 'Expecting 4 addresses to deploy on mainnet')
-  
-  console.log('Deploying Lenia Contract')
-  console.log('Splitees', addresses)
-  console.log('Shares', shares)
-  const deployer = accounts.deployer
-  console.log('Deployer', deployer)
-  console.log('Is this ok? [y/N]')
-  const { ok } = await prompt.get(['ok']);
-  if (ok !== 'y') {
-    console.log('Quitting!')
-    process.exit()
+    console.log('Deployer', deployer)
+    console.log('Is this ok? [y/N]')
+    const { ok } = await prompt.get(['ok']);
+    if (ok !== 'y') {
+      console.log('Quitting!')
+      process.exit()
+    }
+  } else {
+    addresses = [
+      accounts.deployer,
+      accounts.admin1,
+      accounts.admin2,
+      accounts.admin3,
+    ]
+    shares = [1, 2, 3, 4]
   }
   
   const leniaDescriptorLibrary = await deploy("LeniaDescriptor", {
--- deploy/deploy-royalties-contract.js
+++ deploy/deploy-royalties-contract.js
@@ -8,8 +8,9 @@ const deployLeniaRoyaltiesContract = async hre => {
   const {deploy} = deployments
   
   const accounts = await getNamedAccounts()
-  const addresses = []
-  const shares = []
+  const deployer = accounts.deployer
+  let addresses = []
+  let shares = []
 
   if (hre.hardhatArguments.network == 'mainnet' || hre.hardhatArguments.network == 'rinkeby') {
     splitter = JSON.parse(fs.readFileSync(path.join(__dirname, `../tasks/data/royalties-payment-splitter-${hre.hardhatArguments.network}.json`), 'utf8'))
@@ -17,23 +18,29 @@ const deployLeniaRoyaltiesContract = async hre => {
       addresses.push(splitter[i].address)
       shares.push(splitter[i].shares)
     }
+    assert(Object.keys(addresses).length == 4, 'Expecting 4 addresses to deploy on mainnet')
+
+    console.log('Deploying LeniaRoyalties Contract')
+    console.log('Splitees', addresses)
+    console.log('Shares', shares)
+    const deployer = accounts.deployer
+    console.log('Deployer', deployer)
+    console.log('Is this ok? [y/N]')
+    const { ok } = await prompt.get(['ok']);
+    if (ok !== 'y') {
+      console.log('Quitting!')
+      process.exit()
+    }
   } else {
-    //addresses = await getNamedAccounts()
+    addresses = [
+      accounts.deployer,
+      accounts.admin1,
+      accounts.admin2,
+      accounts.admin3,
+    ]
+    shares = [1, 2, 3, 4]
   }
-    
-  assert(Object.keys(addresses).length == 4, 'Expecting 4 addresses to deploy on mainnet')
-  console.log('Deploying LeniaRoyalties Contract')
-  console.log('Splitees', addresses)
-  console.log('Shares', shares)
-  const deployer = accounts.deployer
-  console.log('Deployer', deployer)
-  console.log('Is this ok? [y/N]')
-  const { ok } = await prompt.get(['ok']);
-  if (ok !== 'y') {
-    console.log('Quitting!')
-    process.exit()
-  }
-  
+
   await deploy('LeniaRoyalties', {
     args: [addresses, shares],
     from: deployer,
