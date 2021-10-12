64b76caffa6c3ebdb4425b8118911fb95baa8ddc
Alex Orange <alex.orange.stockmouton@gmail.com>
2021-10-12 15:46:49 +0200

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,6 +37,26 @@ module.exports = {
         allowList: []
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-0CJ4XH940H", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
