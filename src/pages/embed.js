27f24321cf13759e859dab4a18c57efc5ada65b6
Morgan Giraud <contact@morgangiraud.com>
2021-10-28 11:49:47 +0200

    Add embed page

---
+++ src/pages/embed.js
@@ -0,0 +1,23 @@
+import React from "react"
+import styled from "styled-components"
+import { useQueryParam, NumberParam } from "use-query-params";
+
+const Video = styled.video`
+    width: 100%    !important;
+    height: auto   !important;
+`
+const EmbedPage = () => {
+    let [id] = useQueryParam("id", NumberParam);
+    if (typeof id === 'undefined') {
+        id = 0;
+    }
+
+    return (
+        <Video key={id} preload='auto' loop autoPlay muted playsInline={true}>
+            <source src={`${window.location.origin}/metadata/${id}.mp4`} type="video/mp4" />
+            <p>Couldn't load this Lenia, please use a better browser ;)</p>
+        </Video>
+    )
+}
+
+export default EmbedPage
