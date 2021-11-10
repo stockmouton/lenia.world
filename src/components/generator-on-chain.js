a5d5d016e3dc7b093d1fd1b4c34be0a8041faf67
Morgan Giraud <contact@morgangiraud.com>
2021-11-10 10:13:16 +0100

    Update generator on chain to use the ethereum cloudflare gateway

--- src/components/generator-on-chain.js
+++ src/components/generator-on-chain.js
@@ -1,10 +1,9 @@
 import React, { useRef, useEffect, useState } from "react"
 import styled from "styled-components"
 import { simd } from 'wasm-feature-detect';
+import { ethers } from "ethers";
 
-import Toast from './toast'
-import { useLeniaContract } from './lenia-contract-provider'
-import { useWeb3 } from "./web3-provider"
+import artifacts from '../artifacts/mainnet.json'
 import { getEngineCode, getMetadata } from "../utils/sm"
 
 const StyledDiv = styled.div`
@@ -14,64 +13,39 @@ const StyledDiv = styled.div`
   transform: translate(-50%, -50%);
 `
 
-const GeneratorOnChain = ({ zoom, fps, scale, lenia_id }) => {
+const GeneratorOnChain = ({zoom, fps, scale, lenia_id }) => {
     const nodeRef = useRef(null);
-
-    const { isUserMarkedConnected, web3Provider, account } = useWeb3()
-    const { metadataContract } = useLeniaContract()
-    const [ error, setError ] = useState(null)
-
-    let contractSet = useRef(false);
-
+    
+    lenia_id = Math.max(Math.min(lenia_id, 201), 0)
     useEffect(async () => {
-        if (!isUserMarkedConnected()) {
-            setError(new Error(
-                `You must connect your account on the <a href="${window.location.origin}">homepage</a> before being able to use the generator on chain`
-            ))
-            return
-        }
+        const provider = new ethers.providers.JsonRpcProvider('https://cloudflare-eth.com');
+        const metadataContract = new ethers.Contract(
+            artifacts.contracts.LeniaMetadata.address, 
+            artifacts.contracts.LeniaMetadata.abi, 
+            provider
+        );
+        const [WASMSource, WASMSIMDSource, engineBytes] = await getEngineCode(provider, metadataContract)
+        const leniaMetadata = await getMetadata(provider, metadataContract, lenia_id)
+        
+        leniaMetadata["config"]["world_params"]["scale"] = scale
 
-        // Account ready and contract received
-        if (metadataContract && account) {
-            if (contractSet.current) {
-                return
-            } else {
-                contractSet.current = true
-            }
+        const hasSIMD = await simd();
+        const WASMKey = hasSIMD ? 'engine-simd' : 'engine'
+        const WASMByteCode = hasSIMD ? WASMSIMDSource : WASMSource
 
-            lenia_id = Math.max(Math.min(lenia_id, 201), 0)                
-
-            const hasSIMD = await simd();
-            const WASMKey = hasSIMD
-                ? 'engine-simd'
-                : 'engine'
-            const [WASMSource, WASMSIMDSource, engineBytes] = await getEngineCode(web3Provider, metadataContract)
-            const WASMByteCode = hasSIMD ? WASMSIMDSource : WASMSource
-            const engine = engineBytes.toString('utf-8')
-            if (typeof engine === 'string' && engine.length > 0) {
-                var script = document.createElement('script');
-                script.innerHTML = engine
-                document.body.appendChild(script);
-            }
-
-            const leniaMetadata = await getMetadata(web3Provider, metadataContract, lenia_id)
-            leniaMetadata["config"]["world_params"]["scale"] = scale
-            
+        const engine = engineBytes.toString('utf-8')
+        if (engine.length > 0) {
+            var script = document.createElement('script');
+            script.innerHTML = engine
+            document.body.appendChild(script);
             window.leniaEngine.init(WASMByteCode, WASMKey, leniaMetadata, zoom, fps);
         }
-    }, [account, metadataContract])
-    
-    const handleToastClose = () => {
-        setError(null)
-    }
+    }, [])
 
     return (
-        <>
-            <StyledDiv ref={nodeRef}>
-                <canvas id="RENDERING_CANVAS"></canvas>
-            </StyledDiv>
-            {error && <Toast type="error" onClose={handleToastClose}><div dangerouslySetInnerHTML={ { __html: error.message } }></div></Toast>}
-        </>
+        <StyledDiv ref={nodeRef}>
+            <canvas id="RENDERING_CANVAS"></canvas>
+        </StyledDiv>
     )
 }
 
